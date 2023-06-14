ccf19e91a0ca2d81e45f5796e8dd745f2217e159
destiny2222 <oghenekumeerieme@gmail.com>
2023-06-14 17:03:35 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// const Open = document.querySelector(".openbell");
+// const Close = document.querySelector(".Closemodel");
+// const CloseIcon = document.querySelector("#close_icon");
+
+// Open.addEventListener('click', function () {
+//     if (!Close.classList.contains("notify-open")) {
+//         Close.classList.add("notify-open");
+//     }
+// });
+
+// CloseIcon.addEventListener('click', function () {
+//     if (Close.classList.contains("notify-open")) {
+//         Close.classList.remove("notify-open");
+//     }
+// });
